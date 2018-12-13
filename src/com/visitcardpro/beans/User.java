8affe28e403549984e65715153e9e9552bdb7268
B0berman <hugo.walbecq@gmail.com>
2018-12-13 23:01:00 +0100

    user rm jsonignore

--- src/com/visitcardpro/beans/User.java
+++ src/com/visitcardpro/beans/User.java
@@ -3,9 +3,7 @@ package com.visitcardpro.beans;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class User {
-	@JsonIgnore
 	protected long id;
-	@JsonIgnore
 	protected Authentication auth;
 
 	public User() { }
